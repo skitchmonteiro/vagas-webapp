66b0dcb1277662e4125a91b792667a108b121db5
skitchmonteiro <skitchmonteiro@hotmail.com>
2024-11-01 17:20:49 -0300

    changes error msg

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function ErrorPage() {
     return (
         <main>
-            <h1>Esta página não existe</h1>
+            <h1>Esta página não existe, tente novamente</h1>
             <div style={{ padding: '1rem 0' }}></div>
             <Link to="/">Voltar para Tela Inicial?</Link>
         </main>
